b4f98170fb9c4a8660e3488357ac983ebe034da5
kara fraine <kara.fraine@smail.astate.edi>
2020-03-03 11:16:17 -0600

    Initial Commit

---
+++ theDeminsonalRift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theDeminsonalRift
+//
+//  Created by Kara.Fraine on 3/3/20.
+//  Copyright © 2020 Kara.Fraine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
